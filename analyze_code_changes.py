bfb5850ed7788d8a243e1d1d0f22cf4232a4f7a1
Sergey <6213510+sshnaidm@users.noreply.github.com>
2023-04-14 04:31:28 +0300

    Fix small issues and improve prompt (#3)

--- analyze_code_changes.py
+++ analyze_code_changes.py
@@ -24,8 +24,12 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 
 # Analyze the code changes with OpenAI
 model_engine = os.environ["MODEL"]
+commit_title = os.environ["COMMIT_TITLE"]
+commit_message = os.environ["COMMIT_BODY"]
 code = sys.stdin.read()
-prompt = os.environ["PROMPT"] + "\n```\n" + code + "\n```"
+header = (f"Commit title is '{commit_title}'\n"
+          f"and commit message is '{commit_message}'\n")
+prompt = os.environ["PROMPT"] + "\nCode of commit is:\n```\n" + code + "\n```"
 if len(prompt) > 4000:
     print(f"Prompt too long for OpenAI: {len(prompt)} characters, "
           "sending only first 4000 characters")
@@ -37,7 +41,7 @@ kwargs['max_tokens'] = 1024
 kwargs['messages'] = [
     {"role": "system",
      "content": "You are a helpful assistant and code reviewer."},
-    {"role": "assistant", "content": ""},
+    {"role": "assistant", "content": "You are code reviewer for a project."},
     {"role": "user", "content": prompt},
 ]
 try:
