e1a8cf1e50ea8ae1bd6e4e3d6ad23ff91596479b
Roman Pavelka <ChaoticRoman@users.noreply.github.com>
2025-01-12 13:06:57 +0100

    Fix "proxies" bug, add testing and update dependencies (#14)
    * Add simple test
    
    * Update dependencies
    
    * Improve similarity of test run and real run with specified Python version

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export OPENAI_API_KEY=${OPENAI_API_KEY:-$(cat .api_key)}
+export MODEL=gpt-4o-mini
+export COMMIT_TITLE=test
+export COMMIT_BODY=test
+export MAX_LENGTH=256
+export PROMPT="Follow instructions that are introduced as a code."
+
+python3.10 -m venv .venv
+source .venv/bin/activate
+python3.10 -m pip install -r requirements.txt
+
+echo 'Say "It works!", nothing else, please.' | python3.10 analyze_code_changes.py
+
+deactivate
+rm -rf .venv
